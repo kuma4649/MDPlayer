d61859ad7956545dbad67487c0bb6306e766e927
kumatan <kumatan@kumymelody.jp>
2021-02-20 19:38:29 +0900

    MGSDRVlicenses更新

--- MDPlayer/makeZip.bat
+++ MDPlayer/makeZip.bat
@@ -25,7 +25,7 @@ cd
 del /Q .\output\*.pdb
 rem del /Q .\output\*.config
 del /Q .\output\*.bat
-del /Q .\output\MGSDRV.COM
+rem del /Q .\output\MGSDRV.COM
 copy /Y MDPlayer\removeZoneIdent.bat   .\output
 
 rem -- withoutVSTŒü‚¯
