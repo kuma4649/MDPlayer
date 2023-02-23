44f486c7f16d831d8dda025239b8f28f8cf87db2
kumatan <kumatan@kumymelody.jp>
2023-02-23 13:05:28 +0900

    STBL280 2023/02/23   インストーラ作成しやすいようにちょっと調整

--- MDPlayer/removeZoneIdent.bat
+++ MDPlayer/removeZoneIdent.bat
@@ -1,13 +1,14 @@
 @echo off
 echo zip,dll,exeファイルのZone識別子を削除します。
-pause
 
 echo on
-FOR %%a in (*.zip *.dll *.exe) do (echo . > %%a:Zone.Identifier)
-FOR %%a in (ja\*.zip ja\*.dll ja\*.exe) do (echo . > %%a:Zone.Identifier)
-FOR %%a in (plugin\driver\*.zip plugin\driver\*.dll plugin\driver\*.exe) do (echo . > %%a:Zone.Identifier)
-@echo off
 
+DIR /B /S > dir.txt
+FOR /f %%a in (dir.txt) do (
+echo . > %%a:Zone.Identifier
+)
+DEL dir.txt
+
+@echo off
 echo 完了しました。
-pause
 echo on
